78267915d360f320130980baaa06f2872fe60447
LuiggiP <u202218996@upc.edu.pe>
2024-06-19 00:01:38 -0500

    refactor(application): add JpaAuditing

--- src/main/java/com/opensource/securcar_platform/SecurcarPlatformApplication.java
+++ src/main/java/com/opensource/securcar_platform/SecurcarPlatformApplication.java
@@ -2,8 +2,10 @@ package com.opensource.securcar_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Enable JPA Auditing
 public class SecurcarPlatformApplication {
 
 	public static void main(String[] args) {
